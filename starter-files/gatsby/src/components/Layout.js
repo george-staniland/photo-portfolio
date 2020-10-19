59a902c3027a845d0be618ad509d560660eff67d
George-Staniland <georgestaniland@gmail.com>
2020-10-20 12:46:11 +1300

    add styled component to Nav

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Nav />
-      {children}
-    </div>
+    <>
+      <GlobalStyles />
+      <Typography />
+      <ContentStyles>
+        <Nav />
+        {children}
+      </ContentStyles>
+    </>
   );
 }
--- starter-files/gatsby/src/components/Nav.js
+++ starter-files/gatsby/src/components/Nav.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+  margin-top: 4rem;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    text-align: center;
+    list-style: none;
+
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+    color: #537db9;
+    font-weight: 500;
+    letter-spacing: 1.1px;
+    &:hover {
+      color: grey;
+    }
+  }
+`;
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Selected Works</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/commissioned">Commissioned</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <NavStyles>
+        <h1>George Staniland</h1>
+        <ul>
+          <li>
+            <Link to="/">Selected Works</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/commissioned">Commissioned</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </NavStyles>
+    </>
   );
 }
